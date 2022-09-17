efd46013c2b8d1df1c3b03aec42a2a197eb82226
ftdt800 <79777228+ftdt800@users.noreply.github.com>
2022-09-17 15:06:46 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+﻿import time
+import pyperclip
+import keyboard
+from keyboard import press
+from pyrogram import Client, types, filters, enums
+chat_id = 1019975265 # Don't edit
+app = Client(name='sessionon', api_id=ID_HERE, api_hash='HASH_HERE') #сюды надо писать
+def ctrl_key(key):
+    keyboard.press(29)
+    time.sleep(0.02)
+    keyboard.press(key)
+    time.sleep(0.02)
+    keyboard.release(29)
+    keyboard.release(key)
+    time.sleep(0.02)
+async def main():
+    async with app:
+        await app.send_message("@PorfBot", text)
+        time.sleep(5)
+        async for message in app.search_messages(chat_id, limit=1):
+            print(message.text)
+            pyperclip.copy(message.text)
+            ctrl_key(47)
+            if not message.text == text:
+                press('enter')
+while 1==1:
+    keyboard.wait('Ctrl + C')
+    text = pyperclip.paste()
+    app.run(main())
---
+++ start1.bat
@@ -0,0 +1,6 @@
+pip install tgcrypto
+pip install datetime
+pip install pyrogram
+pip install asyncio
+pip install cryptg
+python main.py
